1806d282b67c329b91cce14a586e178faf18f2fe
Daiki.Sanno <dsanno@adachi.ne.jp>
2013-01-03 22:28:42 +0900

    add.create new project. command: rails new mongodb_test --skip-active-record

---
+++ mongodb_test/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MongodbTest::Application.config.secret_token = '874dc5c2631085bf24fb3e907a6b442c49be5b5b530111894d3f41383d3febbe184954d946137ecf0841bf95a236330a7aa01047cb60505b41890a16bd1bf133'
