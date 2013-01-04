8a530c0c2550676f56ed6d528e509f5c4742918e
Daiki.Sanno <dsanno@adachi.ne.jp>
2013-01-05 02:14:12 +0900

    add.create new project. command: rails new mongodb_enquete --skip-active-record

---
+++ mongodb_enquete/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MongodbEnquete::Application.config.secret_token = 'db55fea28821b4471e65166fc58177934ee5250a722cd9471c8e3a42344d98b639f540fa055c4cf1499c46ceda05bf18f30bbe1dad0b60a4dc03660d701cd2f6'
